76ffff930b750166fc3f6740be1378bfc23dab84
SaqibAleem <saqib.py@gmail.com>
2020-04-19 00:55:03 +0500

    Modified Excel file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import twint
 import pandas as pd
 
 
-usernames=pd.read_excel('usernames.xlsx').usernames.map(lambda x: x.lstrip('@')).values
+usernames=pd.read_excel('username.xlsx').usernames.map(lambda x: x.lstrip('@')).values
 
 
 for username in usernames:
@@ -10,5 +10,5 @@ for username in usernames:
     c.Username = username
     c.Since='2020-03-01 01:00:00'
     c.Store_csv = True
-    c.Output = '/CSVs/{}.csv'.format(username)
+    c.Output = 'CSVs/{}.csv'.format(username)
     twint.run.Search(c)
